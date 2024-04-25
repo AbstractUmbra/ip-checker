496c56747212d2752013d2ff048ee90556567dc7
Alex Nørgaard <umbra@abstractumbra.dev>
2024-04-25 21:11:57 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM rust:latest as builder
+
+ARG APP_NAME=ip-checker
+ENV APP_NAME=${APP_NAME}
+
+WORKDIR /app
+
+RUN apt-get update -y && apt-get upgrade -y && apt-get install musl-tools libssl-dev -y && rm -rf /var/lib/apt/lists/*
+
+COPY Cargo.toml Cargo.lock ./
+RUN mkdir src/ && echo "fn main() {}" > src/main.rs
+RUN rustup target add x86_64-unknown-linux-musl
+
+RUN cargo build --release --target x86_64-unknown-linux-musl
+COPY src src
+RUN touch src/main.rs
+RUN cargo build --release --target x86_64-unknown-linux-musl
+
+RUN strip target/x86_64-unknown-linux-musl/release/$APP_NAME
+
+
+FROM alpine:latest
+
+LABEL org.opencontainers.image.source=https://github.com/abstractumbra/ip-checker
+LABEL org.opencontainers.image.description="Home network IP checker."
+LABEL org.opencontainers.image.licenses=ARR
+
+ARG APP_NAME=ip-checker
+ENV APP_NAME=${APP_NAME}
+ENV ROCKET_PROFILE=release
+
+WORKDIR /app
+COPY --from=builder /app/target/x86_64-unknown-linux-musl/release/$APP_NAME .
+
+CMD ./$APP_NAME
---
+++ src/main.rs
@@ -0,0 +1,132 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::net::IpAddr;
+use std::path::PathBuf;
+use tokio::fs;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Config {
+    url: String,
+    ip: String,
+    api_key: String,
+}
+
+#[derive(Serialize, Debug, Clone, Default)]
+struct UpdatePayload {
+    content: String,
+    name: String,
+    r#type: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct UpdateResponseMessage {
+    code: u64,
+    message: String,
+}
+#[derive(Deserialize, Debug)]
+struct UpdateResponseResultMeta {
+    auto_added: Option<bool>,
+    source: Option<String>,
+}
+#[derive(Deserialize, Debug)]
+struct UpdateResponseResult {
+    content: String,
+    name: String,
+    proxied: Option<bool>,
+    r#type: String,
+    comment: Option<String>,
+    created_on: String,
+    id: String,
+    locked: bool,
+    meta: Option<UpdateResponseResultMeta>,
+    modified_on: String,
+    proxiable: bool,
+    tags: Option<Vec<String>>,
+    ttl: Option<u64>,
+    zone_id: Option<String>,
+    zone_name: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct UpdateResponse {
+    errors: Vec<UpdateResponseMessage>,
+    messages: Vec<UpdateResponseMessage>,
+    success: bool,
+    result: UpdateResponseResult,
+}
+
+async fn get_config() -> Result<Config, serde_json::Error> {
+    let path = PathBuf::from("./config.json");
+    let contents = fs::read_to_string(path).await.expect("File is corrupt");
+
+    serde_json::from_str(contents.as_str())
+}
+
+async fn update_config(mut config: Config, new_ip: String) {
+    println!("Writing new IP to config.");
+    config.ip = new_ip;
+    fs::write(
+        PathBuf::from("./config.json"),
+        serde_json::to_string::<Config>(&config).expect("Unable to serialize config."),
+    )
+    .await
+    .expect("Unable to write JSON file.");
+}
+
+async fn get_current_ip() -> Result<(), Box<dyn std::error::Error>> {
+    let config = get_config().await?;
+
+    println!("Attempting bind.");
+
+    let current_ip = reqwest::Client::builder()
+        .local_address(IpAddr::from([0, 0, 0, 0]))
+        .build()?
+        .get("https://ifconfig.me")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    if config.ip == current_ip {
+        println!("No change.");
+        Ok(())
+    } else {
+        post_updated_ip(config, current_ip).await
+    }
+}
+
+async fn post_updated_ip(config: Config, new_ip: String) -> Result<(), Box<dyn std::error::Error>> {
+    println!("IP changed! {} -> {}", config.ip, new_ip);
+
+    let json_payload = UpdatePayload {
+        content: new_ip.clone(),
+        name: "home".to_owned(),
+        r#type: "A".to_owned(),
+    };
+
+    let conf = config.clone();
+
+    println!("Pushing new IP.");
+    let response = reqwest::Client::new()
+        .patch(conf.url)
+        .header("Authorization", format!("Bearer {}", conf.api_key))
+        .json::<UpdatePayload>(&json_payload)
+        .send()
+        .await?
+        .json::<UpdateResponse>()
+        .await?;
+    println!("Pushed new IP.");
+
+    update_config(config, response.result.content).await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    loop {
+        get_current_ip().await?;
+        println!("Finished iteration, sleeping for 5m");
+        tokio::time::sleep(std::time::Duration::from_secs(3600)).await;
+    }
+}
